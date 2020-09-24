85c1b3df34cc1a980ed11253ab2fb93af0ec2da2
Mr-DareDevil <arnabsen1729@gmail.com>
2020-09-24 21:32:10 +0530

    Final Commit

--- src/bot.js
+++ src/bot.js
@@ -1,175 +1,237 @@
 require('dotenv').config();
 
-const {Client} = require('discord.js');
-const Discord = require('discord.js')
+const { Client } = require('discord.js');
+const Discord = require('discord.js');
 const client = new Client();
-const PREFIX = "$"
-const {getUpcommingMatch, getPlayerStats} = require('./dataFetch.js')
+const PREFIX = '$';
+const {
+    getUpcommingMatch,
+    getPlayerStats,
+    getLiveData,
+} = require('./dataFetch.js');
 
 const request = require('request');
 const cheerio = require('cheerio');
 
-const PAGE_URL='https://www.iplt20.com/points-table/2020'
+const PAGE_URL = 'https://www.iplt20.com/points-table/2020';
 
+sendLiveData = (data, channel) => {
+    console.log('livedata');
+    const liveEmbed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(data['score'])
+        .setDescription(data['stat'])
+        .setURL('https://www.iplt20.com/')
+        .setThumbnail('https://i.imgur.com/WdkS5wH.jpg')
+        .setTimestamp();
 
-sendStandings =(channel)=>{
-    let table = [], row=[], teamNames=[];
-    try{
-        request(PAGE_URL, (error, response, html)=>{
-            if(!error && response.statusCode == 200){
+    channel.send(liveEmbed);
+};
+
+sendStandings = (channel) => {
+    let table = [],
+        row = [],
+        teamNames = [];
+    try {
+        request(PAGE_URL, (error, response, html) => {
+            if (!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
-                $('.standings-table__team-name--short').each((i, el)=>{
+                $('.standings-table__team-name--short').each((i, el) => {
                     const item = $(el).text();
                     teamNames.push(item);
-                })
+                });
 
-                $('.standings-table td').each((i, el)=>{
+                $('.standings-table td').each((i, el) => {
                     const item = $(el).text();
-                    if(i%12==0){
+                    if (i % 12 == 0) {
                         console.log(i);
-                        row=[];
-                        row.push(item);
-                    }else if(i%12==11){
+                        row = [];
+                        // row.push(item);
+                    } else if (i % 12 == 11) {
                         table.push(row);
-                    }else if(i%12!=1 && i%12!=6 && i%12!=8 && i%12!=9){
+                    } else if (
+                        i % 12 != 1 &&
+                        i % 12 != 6 &&
+                        i % 12 != 5 &&
+                        i % 12 != 8 &&
+                        i % 12 != 9
+                    ) {
                         row.push(item.replace(' ', ''));
                     }
-                })
-                
-                console.log(table);
-                console.log(teamNames);
-                headers = ['#', 'Teams', 'Played', 'Win', 'Lose', 'Tied', 'Net RR', 'Points'];
-                const SPACE = 9;
-                let msg= '```|';
-                for(let i=0; i<headers.length; i++){
-                    msg+= '\t'+headers[i] + ' '.repeat(SPACE-headers[i].length)+'|';
+                });
+
+                headers = [
+                    'Teams',
+                    'Played',
+                    'Win',
+                    'Lose',
+                    'Net RR',
+                    'Points',
+                ];
+                const SPACE = 6;
+                let msg = '```|';
+                for (let i = 0; i < headers.length; i++) {
+                    msg +=
+                        '  ' +
+                        headers[i] +
+                        ' '.repeat(SPACE - headers[i].length) +
+                        '|';
                 }
                 msg += '\n';
-                for(let i=0; i<headers.length; i++){
-                    msg+= '-'.repeat(SPACE+5);
+                for (let i = 0; i < headers.length; i++) {
+                    msg += '-'.repeat(SPACE + 2);
                 }
                 msg += '\n|';
-                for (let i=0; i<8; i++){
-                    msg += '\t'+teamNames[i]+ ' '.repeat(SPACE-teamNames[i].length)+'|';
-                    for(let j=0; j<table[0].length; j++){
-                        msg += '\t' + table[i][j] + ' '.repeat(SPACE-table[i][j].length)+'|'
+                for (let i = 0; i < 8; i++) {
+                    msg +=
+                        '  ' +
+                        teamNames[i] +
+                        ' '.repeat(SPACE - teamNames[i].length) +
+                        '|';
+                    for (let j = 0; j < table[0].length; j++) {
+                        msg +=
+                            '  ' +
+                            table[i][j] +
+                            ' '.repeat(SPACE - table[i][j].length) +
+                            '|';
                     }
-                    if(i!=7){
+                    if (i != 7) {
                         msg += '\n|';
                     }
-                
                 }
                 msg += '```';
-                channel.send('**IPL 2020 Standings**\n')
+                channel.send('**IPL 2020 Standings**\n');
                 channel.send(msg);
             }
-        })
-    }catch(err){
+        });
+    } catch (err) {
         console.log(err);
         channel.send('> Something went wrong :(');
     }
-}
+};
 
-client.on('ready', ()=>{
-    console.log(`${client.user.username} has joined`)
-})
+client.on('ready', () => {
+    console.log(`${client.user.username} has joined`);
+});
 
-sendUpcommingMatch = (match, channel)=>{
-    const newMatchEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle(`${match['team-1']}  vs   ${match['team-2']}`)
-    channel.send(newMatchEmbed);
-}
-
-sendScheduleMatch = (matches, channel)=>{
+sendScheduleMatch = (matches, channel) => {
     let fields = [];
-    if(matches.length>6){
-        matches=matches.slice(0, 6);
+    if (matches.length > 6) {
+        matches = matches.slice(0, 6);
     }
-    for (match of matches){
+    for (match of matches) {
         fields.push({
             name: match['team-1'],
-            value: `Date: ${new Date(match["dateTimeGMT"]).toLocaleDateString()}`,
-            inline: true
-        })
+            value: `Date: ${new Date(
+                match['dateTimeGMT']
+            ).toLocaleDateString()}`,
+            inline: true,
+        });
         fields.push({
             name: match['team-2'],
-            value: `Time: ${new Date(match["dateTimeGMT"]).toLocaleTimeString()}`,
-            inline: true
-        })
-        fields.push({ name: '\u200B', value: '\u200B' })
+            value: `Time: ${new Date(
+                match['dateTimeGMT']
+            ).toLocaleTimeString()}`,
+            inline: true,
+        });
+        fields.push({ name: '\u200B', value: '\u200B' });
     }
 
     const matchEmbed = new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Upcoming Matches')
-    .setDescription('Team 1  vs  Team 2')
-	.setURL('https://www.iplt20.com/')
-	.setThumbnail('https://i.imgur.com/WdkS5wH.jpg')
-	.addFields(fields)
-	.setTimestamp()
+        .setColor('#0099ff')
+        .setTitle('Upcoming Matches')
+        .setDescription('Team 1  vs  Team 2')
+        .setURL('https://www.iplt20.com/')
+        .setThumbnail('https://i.imgur.com/WdkS5wH.jpg')
+        .addFields(fields)
+        .setTimestamp();
 
     channel.send(matchEmbed);
-}
+};
 
-sendPlayerStats = (stats, channel)=>{
-    try{
-      console.log(stats["imageURL"]);
+sendPlayerStats = (stats, channel) => {
+    try {
+        console.log(stats['imageURL']);
         const matchEmbed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(stats["fullName"])
-        .setDescription(stats["country"])
-        .setURL('https://www.iplt20.com/')
-        .setThumbnail(stats["imageURL"])
-        .addFields([
-            {name: 'Teams', value: stats["majorTeams"].split(',').slice(0, 3).join(',')},
-            {name: 'Playing Role', value: stats["playingRole"]},
-            {name: 'Batting Style', value: stats["battingStyle"], inline: true},
-            {name: 'Batting Style', value: stats["battingStyle"], inline: true},
-            {name: 'Current Age', value: stats["currentAge"]}
+            .setColor('#0099ff')
+            .setTitle(stats['fullName'])
+            .setDescription(stats['country'])
+            .setURL('https://www.iplt20.com/')
+            .setThumbnail(stats['imageURL'])
+            .addFields([
+                {
+                    name: 'Teams',
+                    value: stats['majorTeams'].split(',').slice(0, 3).join(','),
+                },
+                { name: 'Playing Role', value: stats['playingRole'] },
+                {
+                    name: 'Batting Style',
+                    value: stats['battingStyle'],
+                    inline: true,
+                },
+                {
+                    name: 'Batting Style',
+                    value: stats['battingStyle'],
+                    inline: true,
+                },
+                { name: 'Current Age', value: stats['currentAge'] },
             ])
-        .setImage(stats["imageURL"])
-        .setTimestamp()  
+            .setImage(stats['imageURL'])
+            .setTimestamp();
         channel.send(matchEmbed);
-    }catch(err){
+    } catch (err) {
         channel.send('> Sorry something went wrong !!');
     }
-}
+};
 
-client.on('message', (message)=>{
-    if(message.author.bot) return;
-    if(message.content.startsWith(PREFIX)){
-        const [CMD_NAME, ...args] = message.content.trim().substring(PREFIX.length).split(/\s+/);
+client.on('message', (message) => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
         console.log(CMD_NAME, args);
 
-        if(CMD_NAME=='upcoming'){
+        if (CMD_NAME == 'upcoming') {
             let upcommingMatch = getUpcommingMatch();
             sendScheduleMatch(upcommingMatch, message.channel);
-            
-        }else if(CMD_NAME=='player'){
-            if(args.length===0){
+        } else if (CMD_NAME == 'player') {
+            if (args.length === 0) {
                 message.channel.send(
                     '```Enter player name\n Correct syntax: $player <player_name> .\nFor more help type $help```'
-                )
+                );
                 return;
             }
             const name = args.join(' ');
             const playerStat = getPlayerStats(name);
-            if(playerStat==null){
+            if (playerStat == null) {
                 message.channel.send(
                     '```Enter player name\nCorrect syntax: $player <player_name> \nFor more help type $help```'
-                )
+                );
                 return;
             }
             console.log(playerStat);
-            playerStat.then((value)=>{sendPlayerStats(value, message.channel)})
-
-        }else if(CMD_NAME==='help'){
-            message.channel.send('```IPL Notifs Commands:-  \nupcoming:  \tSchedule of all upcoming IPL matched(at most 6)\nlive:  \t\tLive Score\nstandings: \tCurrent Standings\nplayer:    \tPlayer Info e.g $player Patt Cummins```')
-        }else if(CMD_NAME==='standings'){
+            playerStat.then((value) => {
+                sendPlayerStats(value, message.channel);
+            });
+            return;
+        } else if (CMD_NAME === 'help') {
+            message.channel.send(
+                '```IPL Notifs Commands:-  \nupcoming:  \tSchedule of all upcoming IPL matched(at most 6)\nlive:  \t\tLive Score\nstandings: \tCurrent Standings\nplayer:    \tPlayer Info e.g $player Patt Cummins```'
+            );
+            return;
+        } else if (CMD_NAME === 'standings') {
             sendStandings(message.channel);
+            return;
+        } else if (CMD_NAME === 'live') {
+            const liveData = getLiveData();
+            liveData.then((val) => {
+                sendLiveData(val, message.channel);
+            });
+            return;
         }
     }
-})
+});
 
 client.login(process.env.DISCORD_BOT_TOKEN);
--- src/dataFetch.js
+++ src/dataFetch.js
@@ -5,47 +5,67 @@ const schedule = require('../data.json');
 // const API_URL='http://cricscore-api.appspot.com/csa'
 
 const API_KEY = process.env.API_KEY;
-const MATHCES_URL='https://cricapi.com/api/matches?apikey='+API_KEY
-const PLAYERID_URL='https://cricapi.com/api/playerFinder/?apikey='+API_KEY
-const PLAYERSTAT_URL='https://cricapi.com/api/playerStats?&apikey='+API_KEY
+const MATHCES_URL = 'https://cricapi.com/api/matches?apikey=' + API_KEY;
+const PLAYERID_URL = 'https://cricapi.com/api/playerFinder/?apikey=' + API_KEY;
+const PLAYERSTAT_URL = 'https://cricapi.com/api/playerStats?&apikey=' + API_KEY;
+const LIVE_URL = 'https://cricapi.com/api/cricketScore/?apikey=' + API_KEY;
 const TEAMS = [
-'Delhi Capitals',
-'Chennai Super Kings',
-'Kings XI Punjab',
-'Kolkata Knight Riders',
-'Mumbai Indians',
-'Rajasthan Royals',
-'Royal Challengers Bangalore',
-'Sunrisers Hyderabad']
+    'Delhi Capitals',
+    'Chennai Super Kings',
+    'Kings XI Punjab',
+    'Kolkata Knight Riders',
+    'Mumbai Indians',
+    'Rajasthan Royals',
+    'Royal Challengers Bangalore',
+    'Sunrisers Hyderabad',
+];
 
-getUpcommingMatch = ()=>{
+getUpcommingMatch = () => {
     let IPLMatches = [];
-    for (match of schedule){
-        if(new Date(match.dateTimeGMT) > new Date()){
+    for (match of schedule) {
+        if (new Date(match.dateTimeGMT) > new Date()) {
             IPLMatches.push({
-                ...match
-            })
+                ...match,
+            });
         }
     }
     return IPLMatches;
-}
+};
 
-getPlayerStats = async (name)=>{
-    try{
-        const resp = await axios.get(`${PLAYERID_URL}&name=${encodeURI(name)}`)
-        
+getCurrentMatchID = () => {
+    let sameDay = [];
+    for (match of schedule) {
+        if (
+            new Date(match.dateTimeGMT).getDate() == new Date().getDate() &&
+            new Date(match.dateTimeGMT).getTime() <= new Date().getTime()
+        ) {
+            sameDay.push(match.unique_id);
+        }
+    }
+    return sameDay[0];
+};
+
+getLiveData = async () => {
+    const resp = await axios.get(
+        `${LIVE_URL}&unique_id=${getCurrentMatchID()}`
+    );
+    return resp.data;
+};
+
+getPlayerStats = async (name) => {
+    try {
+        const resp = await axios.get(`${PLAYERID_URL}&name=${encodeURI(name)}`);
+
         // resp.then((val)=>{console.log(val)});
         const pid = resp.data.data[0].pid;
         const playerStat = await axios.get(`${PLAYERSTAT_URL}&pid=${pid}`);
 
         return playerStat.data;
-    }catch(err){
+    } catch (err) {
         console.log(err);
         return null;
     }
-}
-
-
+};
 
 // getPlayerStats('Sachin Rana')
-module.exports = {getUpcommingMatch, getPlayerStats}
+module.exports = { getUpcommingMatch, getPlayerStats, getLiveData };
